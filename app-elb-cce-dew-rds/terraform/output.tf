50318a0324a7c3713a298c4e29cdec98f00fe4bf
alejandromore <alejandro.more@huawei.com>
2026-02-06 11:39:40 -0500

    update variable

--- app-elb-cce-dew-rds/terraform/output.tf
+++ app-elb-cce-dew-rds/terraform/output.tf
@@ -19,7 +19,7 @@ output "rds_postgres_password" {
 }
 
 output "elb_public_id" {
-  value = huaweicloud_lb_loadbalancer.elb_public.elb_id
+  value = huaweicloud_lb_loadbalancer.elb_public.id
 }
 
 output "elb_public_ip" {
