021715d89994158c32797241668f8fd0fac53355
alejandromore <alejandro.more@huawei.com>
2026-02-12 10:43:31 -0500

    add demo-client

--- tdp-config-catalog-cce/terraform/output.tf
+++ tdp-config-catalog-cce/terraform/output.tf
@@ -10,10 +10,10 @@
 #  value = module.cce_cluster.cluster_id
 #}
 
-#output "elb_public_id" {
-#  value = huaweicloud_lb_loadbalancer.elb_public.id
-#}
+output "elb_public_id" {
+  value = huaweicloud_lb_loadbalancer.elb_public.id
+}
 
-#output "elb_public_ip" {
-#  value = module.eip_elb_public.address
-#}
+output "elb_public_ip" {
+  value = module.eip_elb_public.address
+}
