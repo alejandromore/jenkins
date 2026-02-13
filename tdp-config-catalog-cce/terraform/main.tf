80df6bb24be80c7c7906bc78e6e8bd082053f3c1
alejandromore <alejandro.more@huawei.com>
2026-02-13 12:02:15 -0500

    add version logic

--- tdp-config-catalog-cce/terraform/main.tf
+++ tdp-config-catalog-cce/terraform/main.tf
@@ -341,9 +341,16 @@ resource "huaweicloud_cce_node_pool" "nodepool" {
   key_pair           = var.key_pair_name
 }
 
+# 1. Buscamos la versión disponible para tu cluster específico
+data "huaweicloud_cce_addon_template" "pip_template" {
+  cluster_id    = huaweicloud_cce_cluster.my_cluster.id
+  template_name = "pip"
+}
+# 2. Instalamos usando la versión que nos devolvió el data source
 resource "huaweicloud_cce_addon" "pod_identity" {
   cluster_id    = module.cce_cluster.cluster_id
   template_name = "pip"
+  version       = data.huaweicloud_cce_addon_template.pip_template.version
 
   values {
     basic = {
