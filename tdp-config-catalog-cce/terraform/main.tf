03904d559eeaa51eb129f75b3091e1cb6800a028
alejandromore <alejandromore@gmail.com>
2026-02-17 22:51:51 -0500

    update

--- tdp-config-catalog-cce/terraform/main.tf
+++ tdp-config-catalog-cce/terraform/main.tf
@@ -454,14 +454,11 @@ resource "huaweicloud_cce_node_pool" "nodepool" {
 #######################################
 # Agency
 #######################################
-# 1. El data source correcto es 'identity_user' (v1.86.0 no tiene identity_account)
-data "huaweicloud_identity_user" "current" {}
-
-# 2. Corregir Agency (Se usa delegated_domain_name en lugar de delegation_domain)
 resource "huaweicloud_identity_agency" "obs_workload_agency" {
   name                  = "cce-workload-agency"
   description           = "Agencia para workloads en CCE"
-  delegated_domain_name = data.huaweicloud_identity_user.current.domain_name
+
+  delegated_domain_name = "hwstaff_intl_a00392472"
 
   enterprise_project_roles {
     enterprise_project = var.enterprise_project_name
@@ -472,7 +469,6 @@ resource "huaweicloud_identity_agency" "obs_workload_agency" {
 #######################################
 # Identity Provider (OIDC)
 #######################################
-# 3. Identity Provider (OIDC) - Mapeo ahora es un atributo JSON interno
 resource "huaweicloud_identity_provider" "cce_oidc" {
   name        = "cce-config-catalog-idp"
   protocol    = "oidc"
@@ -480,9 +476,9 @@ resource "huaweicloud_identity_provider" "cce_oidc" {
 
   access_config {
     # Cambios de nombres de atributos según la v1.86.0
-    provider_url = data.huaweicloud_cce_cluster.cce_cluster_turbo.iam_url
+    provider_url = huaweicloud_cce_cluster.cce_cluster_turbo.iam_url
     client_id    = "sts.myhuaweicloud.com"
-    signing_key  = data.huaweicloud_cce_cluster.cce_cluster_turbo.oidc_config[0].issuer_key
+    signing_key  = huaweicloud_cce_cluster.cce_cluster_turbo.oidc_config[0].issuer_key
   }
 
   # El mapping ya no es un recurso independiente, es un bloque JSON
@@ -501,7 +497,6 @@ resource "huaweicloud_identity_provider" "cce_oidc" {
     }
   ])
 }
-
 
 #######################################
 # DEW - Secret
