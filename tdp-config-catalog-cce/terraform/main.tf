57f227dd95145596fa1b07440d3624dd0702cb19
alejandromore <alejandromore@gmail.com>
2026-02-17 22:39:06 -0500

    workload identity

--- tdp-config-catalog-cce/terraform/main.tf
+++ tdp-config-catalog-cce/terraform/main.tf
@@ -449,66 +449,70 @@ resource "huaweicloud_cce_node_pool" "nodepool" {
   tags = var.tags
 }
 
+
+
 #######################################
 # Agency
 #######################################
-data "huaweicloud_domain" "current" {}
+# Obtiene la información de la cuenta autenticada actualmente
+data "huaweicloud_identity_account" "current" {}
 
-resource "huaweicloud_identity_agency" "obs_workload_agency" {
-  name = "ecs-obs-dew-agency"
-  description = "Agencia para workloads en CCE"
-  
-  domain_id   = data.huaweicloud_domain.current.id
-
-  enterprise_project_roles {
-    enterprise_project = var.enterprise_project_name
-    roles = ["OBS Administrator", "CSMS FullAccess"]
-  }
+# El ID se accede mediante la propiedad .id
+output "account_id" {
+  value = data.huaweicloud_identity_account.current.id
 }
 
+# Agency Única: Permisos para OBS y DEW (CSMS)
+resource "huaweicloud_identity_agency" "workload_agency" {
+  name        = "cce-workload-agency" # Asegura que coincida con el YAML
+  description = "Agencia para workloads OBS y DEW en CCE"
+  
+  delegation_domain {
+    id = data.huaweicloud_identity_account.current.id 
+  }
+
+  # Usamos roles de alcance global o EP según tu necesidad
+  enterprise_project_roles {
+    enterprise_project = var.enterprise_project_name
+    roles              = ["OBS Administrator", "CSMS FullAccess"]
+  }
+}
 
 #######################################
 # Identity Provider (OIDC)
 #######################################
-
-# Crear el Identity Provider para el Clúster CCE
+# Identity Provider (OIDC)
 resource "huaweicloud_identity_provider" "cce_oidc" {
-  name     = "cce-config-catalog-idp"
+  name     = "cce-oidp"
+  protocol = "oidc"
 
   access_config {
+    # El IAM URL se obtiene del cluster CCE
     idp_url   = data.huaweicloud_cce_cluster.cce_cluster_turbo.iam_url
     client_id = "sts.myhuaweicloud.com"
   }
 }
 
-# Configuración de Reglas de Conversión
+# Reglas de Mapeo: Conecta SAs específicos con la Agency
 resource "huaweicloud_identity_mapping" "cce_sa_mapping" {
   identity_provider_id = huaweicloud_identity_provider.cce_oidc.id
 
-  # Regla para el SA de OBS
+  # Regla unificada para ambos ServiceAccounts
   rules {
     local {
-      agency = huaweicloud_identity_agency.obs_workload_agency.name
+      agency = huaweicloud_identity_agency.workload_agency.name
     }
     remote {
       attribute = "sub"
       condition = "anyOf"
-      value     = ["system:serviceaccount:default:sa-obs"]  #default es el namespace
-    }
-  }
-
-  # Regla para el SA de DEW
-  rules {
-    local {
-      agency = huaweicloud_identity_agency.obs_workload_agency.name
-    }
-    remote {
-      attribute = "sub"
-      condition = "anyOf"
-      value     = ["system:serviceaccount:default:sa-dew"]  #default es el namespace
+      value     = [
+        "system:serviceaccount:default:sa-obs",
+        "system:serviceaccount:default:sa-dew"
+      ]
     }
   }
 }
+
 
 #######################################
 # DEW - Secret
