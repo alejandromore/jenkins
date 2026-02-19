4e78ffd1b4cac925167923cbc705709cf243fa5a
alejandromore <alejandromore@gmail.com>
2026-02-18 22:35:06 -0500

    Secrets Store

--- tdp-config-catalog-cce/helm/busybox/secrets-store/readme.md
+++ tdp-config-catalog-cce/helm/busybox/secrets-store/readme.md
@@ -1,16 +1,24 @@
 Patron: Secrets Store CSI Driver with Azure Key Vault
+Service Principal legacy (client_id + client_secret)
 
-Azure
- + AKS
- + Secrets Store CSI Driver
- + Azure Key Vault
- + Managed Identity
- + (Key Vault guarda client_id + client_secret de un Service Principal)
+Azure Legacy	   | Huawei Equivalente
+---------------------------------------
+Service Principal  | IAM User
+client_id	       | Access Key
+client_secret	   | Secret Key
+Managed Identity   | Agency
+Key Vault	       | CSMS
 
-Huawei
-+ CCE
-+ Secrets Store CSI Driver
-+ CSMS
-+ Agency
-+ (CSMS guarda AK/SK de un IAM User)
+[terraform] crear usuario IAM y privilegios
+[terraform] crear CSMS (DEW) guardando AK y SK
+[helm] instalar add on Secrets Store CSI Driver
 
+$env:KUBECONFIG="C:\Users\A00392472\Downloads\cce-config-catalog-kubeconfig.yaml"
+$env:KUBECONFIG="C:\Users\Huawei\Downloads\cce-config-catalog-kubeconfig.yaml"
+
+# Secrets Store CSI Driver
+CCE Secrets Manager for DEW
+
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+
+helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver --namespace kube-system
--- tdp-config-catalog-cce/helm/busybox/secrets-store/terraform.tf
+++ tdp-config-catalog-cce/helm/busybox/secrets-store/terraform.tf
@@ -1,15 +1,71 @@
 #######################################
-# Agency
+# Crear IAM User
 #######################################
-resource "huaweicloud_identity_agency" "identity_agency" {
-  name                  = "cce-workload-agency"
-  description           = "Agencia para el CCE"
-  delegated_service_name = "op_svc_cce"
+resource "huaweicloud_identity_user" "cce_programmatic_user" {
+  name        = "cce-programmatic-user"
+  description = "IAM user for legacy pattern from CCE"
+  enabled     = true
+}
+
+resource "huaweicloud_identity_access_key" "cce_user_key" {
+  user_id = huaweicloud_identity_user.cce_programmatic_user.id
+}
+
+#######################################
+# Asignar privilegios al IAM User
+#######################################
+data "huaweicloud_identity_role" "obs_operate" {
+  name = "OBS OperateAccess"
+}
+
+resource "huaweicloud_identity_user_role_assignment" "obs_role_attach" {
+  user_id = huaweicloud_identity_user.cce_programmatic_user.id
+  role_id = data.huaweicloud_identity_role.obs_operate.id
+}
+
+data "huaweicloud_identity_role" "csms_secret_user" {
+  name = "CSMS Secret User"
+}
+
+resource "huaweicloud_identity_user_role_assignment" "csms_role_attach" {
+  user_id = huaweicloud_identity_user.cce_programmatic_user.id
+  role_id = data.huaweicloud_identity_role.csms_secret_user.id
+}
+
+#######################################
+# Modificar Agency
+#######################################
+data "huaweicloud_identity_agency" "cce_cluster_agency" {
+  name = "cce_cluster_agency"
+}
+
+resource "huaweicloud_identity_agency" "cce_cluster_agency" {
+  agency_name       = data.huaweicloud_identity_agency.cce_cluster_agency.name
 
   enterprise_project_roles {
     enterprise_project = var.enterprise_project_name
-    roles              = ["OBS Administrator", "CSMS FullAccess"]
+    roles              = ["CSMS Secret User"]
   }
 }
 
+#######################################
+# Instalar Add On
+#######################################
+resource "huaweicloud_cce_addon" "secrets_manager_dew" {
+  cluster_id    = huaweicloud_cce_cluster.cce_cluster_turbo.id
+  template_name = "cce-secrets-manager"
+  version       = "latest"
 
+  # Valores opcionales del add-on
+  # rotation_poll_interval es el periodo de sincronización de secretos, en minutos
+  values {
+    basic = {
+      rotation_poll_interval = "2m"
+    }
+  }
+
+  # Asegura aplicar después del cluster
+  depends_on = [
+    huaweicloud_cce_cluster.cce_cluster_turbo
+  ]
+}
