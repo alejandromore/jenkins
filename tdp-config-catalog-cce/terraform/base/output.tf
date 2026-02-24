15b4f8cfc60e20414874367098c0151d6a1c07b3
alejandromore <alejandromore@gmail.com>
2026-02-23 23:50:03 -0500

    add sa and spc

--- tdp-config-catalog-cce/terraform/base/output.tf
+++ tdp-config-catalog-cce/terraform/base/output.tf
@@ -17,3 +17,8 @@ output "elb_public_id" {
 output "elb_public_ip" {
   value = module.eip_elb_public.address
 }
+
+output "kubeconfig" {
+  value     = huaweicloud_cce_cluster.cce_cluster_turbo.kube_config_raw
+  sensitive = true
+}
