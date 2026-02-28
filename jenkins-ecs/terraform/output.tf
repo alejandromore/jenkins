64d5ddb61ca538f41b4e07878bb4ffb5fd93b6c4
alejandromore <alejandromore@gmail.com>
2026-02-28 17:50:44 -0500

    add variable

--- jenkins-ecs/terraform/output.tf
+++ jenkins-ecs/terraform/output.tf
@@ -5,3 +5,13 @@ output "ecs_name" {
 output "ecs_public_ip" {
   value = module.eip_ecs_publico.address
 }
+
+data "huaweicloud_csms_secret_version" "key_data" {
+  secret_name = var.private_key_name
+}
+
+# Uso opcional: por ejemplo, para pasarla a otra herramienta de gestión
+output "private_key_content" {
+  value     = data.huaweicloud_csms_secret_version.key_data.secret_text
+  sensitive = true
+}
