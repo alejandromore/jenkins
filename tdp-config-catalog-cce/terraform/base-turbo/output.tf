f9fece785ad71d9811b32c5bedd69ad978b33dd8
alejandromore <alejandromore@gmail.com>
2026-03-09 21:07:33 -0500

    save kubeconfig

--- tdp-config-catalog-cce/terraform/base-turbo/output.tf
+++ tdp-config-catalog-cce/terraform/base-turbo/output.tf
@@ -5,3 +5,8 @@ output "elb_public_id" {
 output "elb_public_ip" {
   value = huaweicloud_vpc_eip.eip_elb.address
 }
+
+output "kubeconfig" {
+  value     = huaweicloud_cce_cluster.cce_cluster_turbo.kube_config_raw
+  sensitive = true
+}
